4ba275e51b5712ef0e95e0d8cf96f4b96f89d3a0
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-13 14:00:34 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def name(name1, name2)
+    name1 = Naureen
+    name2 = Jimmy
+end
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+    puts "Hello, Jimmy!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    return num1 + num2
+end
+
+def halve num
+    if num.class != Integer
+         return nil
+    end
+
+        return num/ 2;
+end
